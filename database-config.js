f1d81a1b712d919d56ab794cf69e3070b566c642
SEUNGMIN999 <130059829+SEUNGMIN999@users.noreply.github.com>
2026-09-06 21:25:52 +0900

    코덱스 수정

---
+++ database-config.js
@@ -0,0 +1,13 @@
+/*
+ * 붉은 벽돌 기록관 온라인 연결 설정
+ *
+ * Supabase 대시보드의 Settings > API에서 아래 두 값을 복사하세요.
+ * - supabaseUrl: Project URL
+ * - supabasePublishableKey: Publishable key (또는 구형 anon public key)
+ *
+ * 주의: service_role key와 secret key는 브라우저 파일에 절대 넣지 마세요.
+ */
+window.MOKPO_DATABASE = Object.freeze({
+  supabaseUrl: '',
+  supabasePublishableKey: ''
+});
